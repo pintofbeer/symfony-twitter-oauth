47402f6a802ba6f41dc426e13bbc449643c29d56
David Madelin <pintofcider@gmail.com>
2019-05-10 13:24:39 +0100

    Small update

--- src/SymfonyTwitterOAuthBundle.php
+++ src/SymfonyTwitterOAuthBundle.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Pintofbeer\SymfonyTwitterOAuthBundle;
+namespace Pintofbeer\SymfonyTwitterOAuth;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class SymfonyTwitterOAuthBundle extends Bundle
 {
-	
+	const VERSION = '1.0.0';
 }
